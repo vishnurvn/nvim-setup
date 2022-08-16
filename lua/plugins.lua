79723f1a88faec7d235096d7938454dae3c84b1b
Vishnu Raveendran <vishnunilambur@gmail.com>
2022-08-16 20:57:37 +0530

    first commit

---
+++ lua/plugins.lua
@@ -0,0 +1,15 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+end)
